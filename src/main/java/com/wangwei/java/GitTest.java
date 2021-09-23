a9d668949a9274ead93ac5698d14d4793cbef52f
Wangwei <Wangwei@qq.com>
2021-09-23 19:33:31 +0800

    second commit

--- src/main/java/com/wangwei/java/GitTest.java
+++ src/main/java/com/wangwei/java/GitTest.java
@@ -7,5 +7,6 @@ package com.wangwei.java;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,idea,git");
+        System.out.println("hello,git,remove");
     }
 }
