a138a3cae7defa9898037cff04f9df8716b0b467
Wangwei <Wangwei@qq.com>
2021-09-23 19:40:59 +0800

    third commit

--- src/main/java/com/wangwei/java/GitTest.java
+++ src/main/java/com/wangwei/java/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,idea,git");
         System.out.println("hello,git,remove");
+        System.out.println("hot-fix");
     }
 }
