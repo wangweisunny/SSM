45c6106de3dcd7e25515cfd04f822b567d19d770
Wangwei <Wangwei@qq.com>
2021-09-23 19:31:26 +0800

    first commit

---
+++ src/main/java/com/wangwei/java/GitTest.java
@@ -0,0 +1,11 @@
+package com.wangwei.java;
+
+/**
+ * @author WangWei
+ * @create 2021-09-23 19:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,idea,git");
+    }
+}
