ffb12dfea2a84b2c1815b34ca5da781203e7be63
wangweisunny <91251472+wangweisunny@users.noreply.github.com>
2021-09-24 08:55:23 +0800

    online test

--- src/main/java/com/wangwei/java/GitTest.java
+++ src/main/java/com/wangwei/java/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("这个假装有个大佬对其进行了更改。");
     }
 }
