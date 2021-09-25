a5a327872b77642d047e28a20ec094ea9932587d
Wangwei <Wangwei@qq.com>
2021-09-25 16:14:02 +0800

    gitee commit

--- src/main/java/com/wangwei/java/GitTest.java
+++ src/main/java/com/wangwei/java/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("这个假装有个大佬对其进行了更改。");
+
+        System.out.println("在这个里面，使用gitee进行修改上传。");
     }
 }
